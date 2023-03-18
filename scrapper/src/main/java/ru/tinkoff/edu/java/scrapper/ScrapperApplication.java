a42d8900deb29a64f49e0563d6db2ff31f8b8cb3
aleksseii <alesha.shkel@mail.ru>
2023-03-18 08:53:36 +0300

    Configured SpringBoot applications

---
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -0,0 +1,18 @@
+package ru.tinkoff.edu.java.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ApplicationConfig.class)
+public class ScrapperApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ScrapperApplication.class, args);
+        ApplicationConfig config = context.getBean(ApplicationConfig.class);
+        System.out.println(config);
+    }
+}

4395094162b5d15e309b4b010dd4eb124573356c
aleksseii <alesha.shkel@mail.ru>
2023-03-28 21:38:43 +0300

    Added scheduler interval in scrapper module and changed prefix names in bot and scrapper modules in ApplicationConfig classes

--- bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,11 +1,11 @@
 package ru.tinkoff.edu.java.bot.configuration;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 
 @Validated
-@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotNull String test) {
+@ConfigurationProperties(prefix = "bot", ignoreUnknownFields = false)
+public record ApplicationConfig(@NotBlank String test) {
 }
